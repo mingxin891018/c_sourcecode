9832b1195d598c5816ecff40cab4a638f8e495df
zhaomingxin <zhaomingxin@sunniwell.net>
2019-08-16 09:27:44 +0800

    AES加解密添加ECB和PKCS0模式

--- priv_test/openssl_test/crypto_demo/demo-src/aes_test.c
+++ priv_test/openssl_test/crypto_demo/demo-src/aes_test.c
@@ -17,12 +17,19 @@
 
 //#define DEBUG_PRINT
 
-/*****************************************************************************/
-/* Public functions:padding                                                  */
-/*****************************************************************************/
-unsigned char *padding(const unsigned char *src, size_t src_len, size_t *out_len)
+/*****************************************************************************
+* pkcs_type	: 1 PKCS5 	2 PKCS0 
+* mode		: 1 CBC 	2 ECB
+*****************************************************************************/
+unsigned char *AES128_encrypt(const unsigned char *in, size_t len, size_t *out_len, const unsigned char *key, unsigned char *iv, int pkcs_type, int mode);
+unsigned char *AES128_decrypt(const unsigned char *in, size_t len, size_t *out_len, const unsigned char *key, unsigned char *iv, int pkcs_type, int mode);
+
+/*****************************************************************************
+* Public functions:pkcs5
+* PKCS#5
+*****************************************************************************/
+static unsigned char *pkcs5(const unsigned char *src, size_t src_len, size_t *out_len)
 {
-	// PKCS#5
 	int i = 0;
 	unsigned char *p = NULL;
 	int padd_len = SIZE_BLOCK - (src_len%SIZE_BLOCK);
@@ -44,7 +51,33 @@ unsigned char *padding(const unsigned char *src, size_t src_len, size_t *out_len
 	return p;
 }
 
-void bin_print(const unsigned char *data, size_t len)
+static unsigned char *pkcs0(const unsigned char *src, size_t src_len, size_t *out_len)
+{
+	int i = 0;
+	unsigned char *p = NULL;
+	int padd_len = SIZE_BLOCK - (src_len%SIZE_BLOCK);
+	
+	if((padd_len - src_len) == SIZE_BLOCK)
+		padd_len = 0;
+
+	p = (unsigned char *)malloc(src_len + padd_len);
+	if(!p){
+		log_error("malloc error!\n");
+		*out_len = 0;
+		return NULL;
+	}
+
+	memset(p, 0, src_len + padd_len);
+	memcpy(p, src, src_len);
+
+	for(i = 0; i < padd_len; ++i)
+		p[src_len + i] = 0;
+
+	*out_len = src_len + padd_len;
+	return p;
+}
+
+static void bin_print(const unsigned char *data, size_t len)
 {
 	size_t i;
 	printf("BIN DATA LEN=%zu,DATA:\n", len);
@@ -53,21 +86,30 @@ void bin_print(const unsigned char *data, size_t len)
 	printf("\n");
 }
 
-//加密任意长度的数据,返回值为加解密之后数据的长度
-unsigned char *AES128_CBC_CS5_encrypt(const unsigned char *in, size_t len, size_t *out_len, const unsigned char *key, unsigned char *iv)
+unsigned char *AES128_encrypt(const unsigned char *in, size_t len, size_t *out_len, const unsigned char *key, unsigned char *iv, int pkcs_type, int mode)
 {
 	AES_KEY aes;
 	unsigned char *plaintext = NULL, *p = NULL;
 	size_t plaintext_len = 0;
-	
+
 	if(!in || !out_len || !key || !iv){
 		log_error("param error");
 		return NULL;
 	}
-
-	plaintext = padding(in, len, &plaintext_len);
-	if(plaintext == NULL){
-		log_error("malloc failed!\n");
+	if(pkcs_type == 1){
+		plaintext = pkcs5(in, len, &plaintext_len);
+		if(plaintext == NULL){
+			log_error("malloc failed!\n");
+			goto end;
+		}
+	}else if(pkcs_type == 2){
+		plaintext = pkcs0(in, len, &plaintext_len);
+		if(plaintext == NULL){
+			log_error("malloc failed!\n");
+			goto end;
+		}
+	}else{
+		log_error("pkcs_type error!\n");
 		goto end;
 	}
 
@@ -85,7 +127,16 @@ unsigned char *AES128_CBC_CS5_encrypt(const unsigned char *in, size_t len, size_
 #ifdef DEBUG_PRINT
 	bin_print(plaintext, plaintext_len);
 #endif
-	AES_cbc_encrypt(plaintext, p, plaintext_len, &aes, iv, AES_ENCRYPT);
+	if(mode == 1){
+		AES_cbc_encrypt(plaintext, p, plaintext_len, &aes, iv, AES_ENCRYPT);
+	}else if(mode == 2){
+		size_t i = 0;
+		for(i = 0; i < len; i+=SIZE_BLOCK)
+			AES_ecb_encrypt(plaintext + i, p + i, &aes, AES_ENCRYPT);
+	}else{
+		log_error("encryption mode error!\n");
+		goto end;
+	}
 #ifdef DEBUG_PRINT
 	bin_print(p, plaintext_len);
 #endif
@@ -102,8 +153,7 @@ end:
 	return NULL;
 }
 
-//解密任意长度的数据,返回值为加解密之后数据的长度
-unsigned char *AES128_CBC_CS5_decrypt(const unsigned char *in, size_t len, size_t *out_len, const unsigned char *key, unsigned char *iv)
+unsigned char *AES128_decrypt(const unsigned char *in, size_t len, size_t *out_len, const unsigned char *key, unsigned char *iv, int pkcs_type, int mode)
 {
 	AES_KEY aes;
 	unsigned char *p = NULL;
@@ -140,7 +190,18 @@ unsigned char *AES128_CBC_CS5_decrypt(const unsigned char *in, size_t len, size_
 #ifdef DEBUG_PRINT
 	bin_print(in, len);
 #endif
-	AES_cbc_encrypt(in, p, len, &aes, iv, AES_DECRYPT);
+
+	if(mode == 1){
+		AES_cbc_encrypt(in, p, len, &aes, iv, AES_DECRYPT);
+	}else if(mode == 2){
+		size_t i = 0;
+		for(i = 0; i < len; i+=SIZE_BLOCK)
+			AES_ecb_encrypt(in + i, p + i, &aes, AES_DECRYPT);
+	}else{
+		log_error("encryption mode error!\n");
+		goto end;
+	}
+	
 #ifdef DEBUG_PRINT
 	bin_print(p, len);
 #endif
@@ -150,7 +211,14 @@ unsigned char *AES128_CBC_CS5_decrypt(const unsigned char *in, size_t len, size_
 		goto end;
 	}
 
-	*out_len = len - p[len - 1];
+	if(pkcs_type == 1)
+		*out_len = len - p[len - 1];
+	else if(pkcs_type == 2){
+		*out_len = len;
+	}else{
+		log_error("encryption mode error!\n");
+		goto end;
+	}
 	return p;
 
 end:
@@ -160,6 +228,7 @@ end:
 	return NULL;
 }
 
+#if 1
 int main(int argc, char *argv[])
 {
 	size_t ciphertext_len = 0, plaintext_len = 0;
@@ -171,7 +240,7 @@ int main(int argc, char *argv[])
 	printf("data= %s\n", data);
 	printf("key = %s\n", key);
 	printf("iv  = %s\n\n", iv);
-	ciphertext = AES128_CBC_CS5_encrypt((const unsigned char *)data, strlen(data), &ciphertext_len, (const unsigned char *)key, iv);
+	ciphertext = AES128_encrypt((const unsigned char *)data, strlen(data), &ciphertext_len, (const unsigned char *)key, iv, 1, 1);
 	if(ciphertext == NULL)
 		goto end;
 	printf("=========================================================\n");
@@ -181,7 +250,7 @@ int main(int argc, char *argv[])
 
 	printf("\n\n");
 	unsigned char iv1[32] = "0123456789abcdef";
-	plaintext = AES128_CBC_CS5_decrypt((const unsigned char *)ciphertext, ciphertext_len, &plaintext_len, (const unsigned char *)key, iv1);
+	plaintext = AES128_decrypt((const unsigned char *)ciphertext, ciphertext_len, &plaintext_len, (const unsigned char *)key, iv1, 1, 1);
 	if(plaintext == NULL)
 		goto end;
 	log_info("dec success,plaintext_len=%ld\n", plaintext_len);
@@ -197,5 +266,5 @@ end:
 		free(plaintext);
 	return 0;
 }
-
+#endif
 

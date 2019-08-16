f36f4bfb117a5dcd57558724f67710d26537b12a
zhaomingxin <zhaomingxin@sunniwell.net>
2019-08-16 15:04:13 +0800

    整理目录

--- encrypt/openssl_demo/ssl_test/CA/build_ca.sh
+++ encrypt/openssl_demo/ssl_test/CA/build_ca.sh
@@ -5,5 +5,6 @@ openssl req -new -key ssl.key -subj "/CN=test/OU=t1/O=sunniwell/L=sz/ST=gd/C=cn"
 openssl x509 -req -in ssl.csr -extensions v3_ca -signkey ssl.key -out sign.crt -days 365
 
 
+#-subj '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
 #查看证书信息
 openssl x509 -in sign.crt -noout -serial -subject

c935fd7186ca5c51835be300eb5338681240e393
zhaomingxin <zhaomingxin@sunniwell.net>
2019-08-19 11:43:31 +0800

    消除base64_test.c中的便已警告

--- encrypt/openssl_demo/crypto_test/base64_test.c
+++ encrypt/openssl_demo/crypto_test/base64_test.c
@@ -6,6 +6,7 @@
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 
+//注意，openssl里面的base64编码接口会在编码64个字节以后追加一个\n字符进去
 int base64_encode(char *in_str, int in_len, char *out_str)
 {
 	BIO *b64, *bio;
@@ -33,8 +34,6 @@ int base64_encode(char *in_str, int in_len, char *out_str)
 int base64_decode(char *in_str, int in_len, char *out_str)
 {
 	BIO *b64, *bio;
-	BUF_MEM *bptr = NULL;
-	int counts;
 	int size = 0;
 
 	if (in_str == NULL || out_str == NULL)
@@ -60,11 +59,11 @@ int main()
 	printf("data=%s\n", instr);
 	memset(outstr1, 0, sizeof(outstr1));
 	len1 = base64_encode(instr,sizeof(instr),outstr1);
-	printf("ciphertext length=%d,data:%s\n", len1, outstr1);
+	printf("ciphertext length=%zu,data:%s\n", len1, outstr1);
 
 	char outstr2[1024] = {0};
 	memset(outstr2, 0, sizeof(outstr2));
 	len2 = base64_decode(outstr1,strlen(outstr1),outstr2);
-	printf("plaintext length=%d,data:=%s\n", len2, outstr2);
+	printf("plaintext length=%zu,data:=%s\n", len2, outstr2);
 	return 0;
 }
